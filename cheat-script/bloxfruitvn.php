63f28886cf08cd9973cec50856852c255711c05b
xshibau <ziugpro@gmail.com>
2025-10-18 13:45:34 +0700

    Create bloxfruitvn.php

---
+++ cheat-script/bloxfruitvn.php
@@ -0,0 +1,2 @@
+-- # Aura Hub Blox Fruit VN (Only Blox Fruit It will not be able to use the rest of the script)
+loadstring(game:HttpGet("https://api.junkie-development.de/api/v1/luascripts/public/9001677679b1c27cc98d9ab961b41f3ad7f059426b27934fa7f5b7dbdfa4f884/download"))()
